c252fc997dad1702549d0292a56c78568954362e
v8754105 <ajjekso@gmail.com>
2019-04-24 16:03:02 +0300

    commet 2

--- app/src/main/java/com/example/user/ir/MainActivity.java
+++ app/src/main/java/com/example/user/ir/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.ir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Hello world",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"Hello world",Toast.LENGTH_LONG).show();
+        //j
     }
 }
